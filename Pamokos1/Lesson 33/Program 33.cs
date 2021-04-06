67ac4c41913649f851c4d1cf527ace276decf4ac
DESKTOP-06QQJTI\User <Henrikas.rozenas@gmail.com>
2021-04-06 22:11:31 +0300

    Program 33

---
+++ Pamokos1/Lesson 33/Program 33.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace Pamokos1.Lesson_33
+{
+    class Program_33
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello!!!!");
+        }
+    }
+}
